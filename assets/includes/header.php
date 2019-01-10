a79f88818a81dbf46909818de01b36529eed2e10
lapinell <lapinell@gmail.com>
2019-01-10 15:52:45 -0600

    broke out header and footer into php

---
+++ assets/includes/header.php
@@ -0,0 +1,22 @@
+<?php 
+  include('./assets/includes/arrays.php');
+?>
+
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title></title>
+      <link href="./assets/styles.css" rel="stylesheet">
+    </head>
+
+    <body id="final-example">
+      <div class="wrapper">
+        <div id="banner">
+        </div><!-- banner -->
+          <a href="/" title="Return to Home">
+            <img src="img/banner.png" alt="Franklin's Fine Dining"/>
+          </a>
+        <div id="nav">
+          <?php include('./assets/includes/nav.php'); ?>
+        </div><!-- nav -->
+        <div class="content">
