f99a9584da8c9d00c7fdf44aca864c2878a0cb5c
lapinell <lapinell@gmail.com>
2019-01-28 14:00:28 -0600

    success message

--- contact.php
+++ contact.php
@@ -47,7 +47,7 @@
 
         // If the subsribe checkbox was checked...
 
-        ifisset($_POST['subscribe']) && $_POST['subscribe'] == 'Subscribe') {
+        if( isset( $_POST['subscribe'] ) && $_POST['subscribe'] == 'Subscribe') {
 
           // Add a new line to the $msg
           $msg .= "\r\n\r\nPlease add $email to the mailing list.\r\n";
@@ -61,9 +61,18 @@
         $headers .= "From: $name <$email> \r\n";
         $headers .= "X-Priority: 1\r\n";
         $headers .= "X-MSMail-Priority: High\r\n\r\n";
-      }
+
+        // Send the mail
+        mail ($to, $subject, $msg, $headers);
 
     ?>
+
+    <!-- Show success message -->
+    <h5>Thanks for contacting Franklin's!</h5>
+    <p>Please allow 24 hours for a response.</p>
+    <p><a href="/final" class="button block">&laquo; Go to Home Page</a></p>
+
+    <?php } else { ?>
 
     <form method="post" action="" id="contact-form">
 
@@ -82,6 +91,10 @@
       <input type="submit" class="button next" name="contact_submit" value="Send Message">
 
     </form>
+  
+    <?php } ?>
+
+    <hr>
 
   </div><!-- contact -->
 
